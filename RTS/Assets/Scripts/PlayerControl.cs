0a264b9572d5f675e8af27e0d6349dcd7a23e817
Marcus Callender <m.shute@outlook.com>
2017-08-17 17:23:46 +0100

    Created a selection box by dragging the mouse

--- RTS/Assets/Scripts/PlayerControl.cs
+++ RTS/Assets/Scripts/PlayerControl.cs
@@ -6,8 +6,11 @@ public class PlayerControl : MonoBehaviour
 {
     private Timer m_leftClickTimer;
     private Camera m_cam;
+    private GameObject m_selectionBox;
 
-    private Unit m_selectedUnits;
+    public List<Unit> m_selectedUnits;
+
+    public Vector2 m_selectionBoxStart;
 
     void Start()
     {
@@ -15,16 +18,20 @@ public class PlayerControl : MonoBehaviour
         m_leftClickTimer.m_time = 0.2f;
 
         m_cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        m_selectionBox = GameObject.Find("Selection Box");
+
+        m_selectionBox.SetActive(false);
     }
 
     void Update()
     {
-        Vector3 ray = m_cam.ScreenToWorldPoint(Input.mousePosition);
-        Debug.DrawRay(ray, Vector3.forward * 11, Color.yellow);
+        Vector3 mousePos = m_cam.ScreenToWorldPoint(Input.mousePosition);
+        Debug.DrawRay(mousePos, Vector3.forward * 11, Color.yellow);
 
         if (Input.GetButtonDown("Fire1"))
         {
             m_leftClickTimer.Play();
+            m_selectionBoxStart = mousePos;
         }
 
         m_leftClickTimer.Cycle();
@@ -37,21 +44,37 @@ public class PlayerControl : MonoBehaviour
 
                 RaycastHit hit;
 
-                if (Physics.Raycast(ray, Vector3.forward * 11, out hit))
+                if (Physics.Raycast(mousePos, Vector3.forward * 11, out hit))
                 {
                     Unit _unit = hit.transform.gameObject.GetComponent<Unit>();
-                    
-                    if (_unit)
+
+                    if (_unit && m_selectedUnits.Contains(_unit))
                     {
                         _unit.Select();
+                        m_selectedUnits.Add(_unit);
                     }
                 }
             }
+            else
+            {
+
+            }
+
+            m_selectionBox.SetActive(false);
         }
 
-        if (Input.GetButton("Fire1") && m_leftClickTimer.m_completed)
+        if (Input.GetButton("Fire1"))
         {
-            Debug.Log("Hold");
+            if (m_leftClickTimer.m_completed)
+            {
+                Debug.Log("Hold");
+
+                m_selectionBox.SetActive(true);
+
+                m_selectionBox.transform.position = new Vector3((m_selectionBoxStart.x + mousePos.x) * 0.5f, (m_selectionBoxStart.y + mousePos.y) * 0.5f, m_selectionBox.transform.position.z);
+
+                m_selectionBox.transform.localScale = new Vector3(Mathf.Abs(m_selectionBoxStart.x - mousePos.x), Mathf.Abs(m_selectionBoxStart.y - mousePos.y), m_selectionBox.transform.localScale.z);
+            }
         }
     }
 }
