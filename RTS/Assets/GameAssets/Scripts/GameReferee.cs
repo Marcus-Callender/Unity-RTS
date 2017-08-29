21584bb3cc28fa9afa01ce0f036885ffb56df6ec
Marcus Callender <m.shute@outlook.com>
2017-08-29 19:31:02 +0100

    Added match timer and cleaned prefab folder

---
+++ RTS/Assets/GameAssets/Scripts/GameReferee.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameReferee : MonoBehaviour
+{
+    public float m_timeModifier = 9.1f;
+    public Text m_timeDisplay;
+    private Timer m_gameTimer;
+    public int m_gameStartTime = 99;
+
+    void Start()
+    {
+        m_timeDisplay = GameObject.Find("Game Timer").GetComponent<Text>();
+        m_gameTimer = new Timer();
+        m_gameTimer.m_time = m_gameStartTime * m_timeModifier;
+        
+        m_gameTimer.Play(true);
+    }
+
+    void Update()
+    {
+        m_gameTimer.Cycle();
+        
+        m_timeDisplay.text = "" + Mathf.Ceil(m_gameTimer.GetLerp() * (float)m_gameStartTime);
+    }
+}
