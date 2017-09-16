27159031fd7bec9fdea870fb6a50609c1aba3e96
Marcus Callender <m.shute@outlook.com>
2017-09-16 12:35:33 +0100

    Made mining facilitys turn into mines when destroyed

---
+++ RTS/Assets/GameAssets/Scripts/MiningFacility.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiningFacility : MonoBehaviour
+{
+    public GameObject m_leaveBehind;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        Instantiate(m_leaveBehind, gameObject.transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+    }
+}
