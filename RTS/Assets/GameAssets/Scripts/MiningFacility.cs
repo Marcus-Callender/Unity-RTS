f46a051c523d32913cfefd16a779c671bb2952bd
Marcus Callender <m.shute@outlook.com>
2017-09-16 16:08:57 +0100

    Made mining facilaties add to the players silver

--- RTS/Assets/GameAssets/Scripts/MiningFacility.cs
+++ RTS/Assets/GameAssets/Scripts/MiningFacility.cs
@@ -6,14 +6,41 @@ public class MiningFacility : MonoBehaviour
 {
     public GameObject m_leaveBehind;
 
+    private PlayerControl m_player = null;
+
+    public float m_mineSpeed = 15.0f;
+    private Timer m_mineTimer;
+
     void Start()
     {
+        GameObject playerObj = GameObject.Find("Player");
 
+        if (playerObj && (playerObj.tag == gameObject.tag))
+        {
+            PlayerControl playerData = playerObj.GetComponent<PlayerControl>();
+
+            if (playerData)
+            {
+                m_player = playerData;
+                m_mineTimer = new Timer();
+                m_mineTimer.m_time = m_mineSpeed;
+                m_mineTimer.Play();
+            }
+        }
     }
 
     void Update()
     {
+        if (m_player)
+        {
+            m_mineTimer.Cycle();
 
+            if (m_mineTimer.m_completed)
+            {
+                m_mineTimer.Play();
+                m_player.m_silver++;
+            }
+        }
     }
 
     private void OnDestroy()
