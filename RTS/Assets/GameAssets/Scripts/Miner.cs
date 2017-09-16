5b19337a16d38e0141d45eea8d8c58f971a45f06
Marcus Callender <m.shute@outlook.com>
2017-09-16 11:37:52 +0100

    Made miners transform into a mining facility when the miner colides with a mine

---
+++ RTS/Assets/GameAssets/Scripts/Miner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Miner : MonoBehaviour
+{
+    public GameObject m_toCreate;
+    public string m_interactWithTag = "Mine";
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == m_interactWithTag)
+        {
+            Vector3 createPosition = other.transform.position;
+
+            Instantiate(m_toCreate, createPosition, Quaternion.identity);
+
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
