f00eac1fa71e352f38f2f7fa169fc1134da16b46
Marcus Callender <m.shute@outlook.com>
2018-05-06 11:09:35 +0100

    Imported other hexagon frameworks

--- RTS/Assets/GameAssets/Scripts/hex/CreateHexGrid.cs
+++ RTS/Assets/GameAssets/Scripts/hex/CreateHexGrid.cs
@@ -10,11 +10,28 @@ public class CreateHexGrid : MonoBehaviour
         POINTED_TOP = 0,
         FLAT_TOP = 1
     }
-    
+
+    [Serializable]
+    public struct hexIndex
+    {
+        public int q;
+        public int r;
+
+        public void set(int _x, int _y)
+        {
+            q = _x;
+            r = _y;
+        }
+    }
+
+    [SerializeField]
+    private Camera m_cam;
+
     [SerializeField]
     private GameObject m_hexPrefab;
 
     private GameObject[,] m_createdHexes;
+    private SpriteRenderer[,] m_createdHexSprites;
 
     [SerializeField]
     private E_HexType m_hexType;
@@ -28,15 +45,88 @@ public class CreateHexGrid : MonoBehaviour
     [SerializeField]
     private float m_hexSize = 0.5f;
 
+    private hexIndex m_selectedHexIndex = new hexIndex();
+
+    private float xScreenSize = 17.77778f;
+    private float yScreenSize = 10.0f;
+
+    private int xHexPixelSize;
+    private int yHexPixelSize;
+
+    private float pixelToUU;
+    private float UUToPixel;
+
     void Start()
     {
+        m_selectedHexIndex.set(-1, -1);
         m_createdHexes = new GameObject[m_width, m_height];
+        m_createdHexSprites = new SpriteRenderer[m_width, m_height];
         UpdateGrid();
+
+        xHexPixelSize = Mathf.RoundToInt(m_hexSize * (xScreenSize / m_cam.pixelWidth));
+        yHexPixelSize = Mathf.RoundToInt(m_hexSize * (yScreenSize / m_cam.pixelHeight));
+
+        pixelToUU = yScreenSize / m_cam.pixelHeight;
+        UUToPixel = m_cam.pixelHeight / yScreenSize;
     }
 
     void Update()
     {
+        Layout pointy = new Layout(Layout.pointy, new Point(42, 63), new Point(0, 0));
+        FractionalHex hex = pointy.PixelToHex(new Point(Input.mousePosition.x, Input.mousePosition.y));
+        Debug.Log(hex.q + " : " + hex.r + " : " + hex.s);
 
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartHexPathing(m_cam.ScreenToWorldPoint(Input.mousePosition) * pixelToUU /*- new Vector3(0.5f * xScreenSize, 0.5f * yScreenSize)*/);
+        }
+        else if (Input.GetButtonDown("Fire2"))
+        {
+            ResetHexPaths();
+        }
+    }
+
+    private void StartHexPathing(Vector2 pos)
+    {
+
+
+
+        // convert unity position to hex
+        float x = (Mathf.Sqrt(3) / 3.0f * pos.x - 1.0f / 3.0f * pos.y) / m_hexSize;
+        float y =                                (2.0f / 3.0f * pos.y) / m_hexSize;
+        
+        //float rx = Mathf.Round(x);
+        //float ry = Mathf.Round(y);
+
+        //Vector3 roundedHex = cubeRound(x, -x - y, y);
+        //Vector3 roundedHex = cubeRound(x, -y, -x+y);
+        Vector3 roundedHex = new Vector3(x, y, 0.0f);
+
+        ///roundedHex.x += Mathf.Round(m_width * 0.5f);
+        ///roundedHex.y += Mathf.Round(m_height * 0.5f);
+        //roundedHex.y = m_height - roundedHex.y;
+
+        HexUtils.Hex selectedHex = HexUtils.HexUtils.pixel_to_pointy_hex(roundedHex);
+        //selectedHex.q = m_height - selectedHex.q;
+        Debug.Log("q: " + selectedHex.q + ", r: " + selectedHex.r);
+
+        m_createdHexSprites[(int)selectedHex.q, (int)selectedHex.r].color = Color.red;
+        m_createdHexSprites[(int)selectedHex.r, (int)selectedHex.q].color = Color.yellow;
+
+        //Debug.Log("x: " + (Mathf.Sqrt(3) / 3 * pos.x - 1.0f / 3.0f * pos.y) + "y: " + (2.0f / 3.0f * pos.y));
+    }
+
+    private void ResetHexPaths()
+    {
+        m_selectedHexIndex.set(-1, -1);
+
+        for (int z = 0; z < m_width; z++)
+        {
+            for (int x = 0; x < m_height; x++)
+            {
+                m_createdHexSprites[z, x].color = Color.white;
+            }
+        }
     }
 
     void UpdateGrid()
@@ -47,6 +137,9 @@ public class CreateHexGrid : MonoBehaviour
             {
                 m_createdHexes[z, x] = Instantiate(m_hexPrefab, new Vector3((z * (Mathf.Sqrt(3.0f) * m_hexSize)) + ((Mathf.Sqrt(3.0f) * m_hexSize * 0.5f) * (x % 2)), x * 1.5f * -m_hexSize, 0.0f), Quaternion.identity, transform);
                 m_createdHexes[z, x].transform.localPosition = m_createdHexes[z, x].transform.position;
+                m_createdHexes[z, x].GetComponentInChildren<TextMesh>().text = ("(" + z + ", " + x + ")");
+
+                m_createdHexSprites[z, x] = m_createdHexes[z, x].GetComponent<SpriteRenderer>();
             }
         }
     }
---
+++ RTS/Assets/GameAssets/Scripts/hex/HexUtils.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HexUtils
+{
+    [Serializable]
+    public struct Cube
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Cube(float _x, float _y, float _z)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+    }
+
+    [Serializable]
+    public struct Hex
+    {
+        public float q;
+        public float r;
+
+        public Hex(float _q, float _r)
+        {
+            q = _q;
+            r = _r;
+        }
+    }
+
+    public static class HexUtils
+    {
+        const float size = 0.5f;
+
+        public static Hex cube_to_axial(Cube cube)
+        {
+            float q = cube.x;
+            float r = cube.z;
+            return new Hex(q, r);
+        }
+
+        public static Cube axial_to_cube(Hex hex)
+        {
+            var x = hex.q;
+            var z = hex.r;
+            var y = -x - z;
+            return new Cube(x, y, z);
+        }
+
+        public static Hex cube_to_oddr(Cube cube)
+        {
+            var col = cube.x + (cube.z - Mathf.RoundToInt(Mathf.Abs(cube.z) % 2.0f)) / 2;
+            var row = cube.z;
+            return new Hex(col, row);
+        }
+
+        public static Cube oddr_to_cube(Hex hex)
+        {
+            var x = hex.q - (hex.r - Mathf.RoundToInt(Mathf.Abs(hex.r) % 2.0f)) / 2;
+            var z = hex.r;
+            var y = -x - z;
+            return new Cube(x, y, z);
+        }
+
+        public static Hex pofloaty_hex_to_pixel(Hex hex)
+        {
+            float x = size * (Mathf.Sqrt(3.0f) * hex.q + Mathf.Sqrt(3.0f) / 2.0f * hex.r);
+            float y = size * (3.0f/ 2.0f * hex.r);
+            return new Hex(Mathf.RoundToInt(x), Mathf.RoundToInt(y));
+        }
+
+        public static Hex pixel_to_pointy_hex(Vector2 vec)
+        {
+            var q = Mathf.RoundToInt((Mathf.Sqrt(3.0f) / 3.0f * vec.x - 1.0f/ 3.0f * vec.y) / size);
+            var r = Mathf.RoundToInt((2.0f/ 3.0f * vec.y) / size);
+            return hex_round(new Hex(q, r));
+        }
+
+        public static Cube cube_round(Cube cube)
+        {
+            float rx = Mathf.RoundToInt(cube.x);
+            float ry = Mathf.RoundToInt(cube.y);
+            float rz = Mathf.RoundToInt(cube.z);
+
+            float x_diff = Mathf.Abs(rx - cube.x);
+            float y_diff = Mathf.Abs(ry - cube.y);
+            float z_diff = Mathf.Abs(rz - cube.z);
+
+            if (x_diff > y_diff && x_diff > z_diff)
+                rx = -ry - rz;
+            else if (y_diff > z_diff)
+                ry = -rx - rz;
+            else
+                rz = -rx - ry;
+
+            return new Cube(rx, ry, rz);
+        }
+
+        public static Hex hex_round(Hex hex)
+        {
+            return cube_to_axial(cube_round(axial_to_cube(hex)));
+        }
+    }
+}
