df8ebffb69f3c05f22bdb69e594e7f0cd883132d
Marcus Callender <m.shute@outlook.com>
2018-07-01 12:09:04 +0100

    Added some scripts for use later

---
+++ RTS/Assets/GameAssets/Scripts/hex/HexBuilding.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexBuilding : HexUnit
+{
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+
+    }
+}
